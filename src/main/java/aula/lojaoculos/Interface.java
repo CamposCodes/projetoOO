9519b6db89776fec7e93f25359c63aa2a3fffdcf
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2023-12-08 08:48:35 -0300

    Structuring login page

--- src/main/java/aula/lojaoculos/Interface.java
+++ src/main/java/aula/lojaoculos/Interface.java
@@ -1,12 +1,15 @@
 package aula.lojaoculos;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Interface extends JFrame {
     
     public Interface(){
         setTitle("Loja de Oculos");
-        setVisible(true);
         setSize(800,500);
         //finaliza o programa ao fechar
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,6 +17,37 @@ public class Interface extends JFrame {
         //abre janela no meio da tela
         setLocationRelativeTo(null);
 
+        //Botão login
+        setLayout(null);
+        // cria
+        JButton jButton = new JButton("Entrar");
+        jButton.setBounds(275,200,250,70);
+        jButton.setFont(new Font("Arial", Font.BOLD, 20));
+        jButton.setForeground(new Color(237,241,238));
+        jButton.setBackground(new Color(9,10,9));
+        // adiciona no frame
+        add(jButton);
+        
+        //Ação 
+        jButton.addActionListener(e->{
+            JOptionPane.showMessageDialog(null, "Bem-vindo a loja");         
+        });
+        
+        //caixas de texto
+        JTextField userText = new JTextField("Login");
+        userText.setBounds(275,30,250,50);
+        userText.setFont(new Font("Arial", Font.ITALIC, 20));
+        userText.setText("Login");
+        add(userText);
+        
+        JTextField senhaText = new JTextField("Senha");
+        senhaText.setBounds(275,100,250,50);
+        senhaText.setFont(new Font("Arial", Font.ITALIC, 20));
+        senhaText.setText("Senha");
+        add(senhaText);
+        
+        setVisible(true);
     }
-    
+
+
 }
--- src/main/java/aula/lojaoculos/LojaOculos.java
+++ src/main/java/aula/lojaoculos/LojaOculos.java
@@ -72,7 +72,7 @@ public class LojaOculos {
                         Cliente cliente = new Cliente(cpf, celular, email, nome);
                         vendedor.cadastrarCliente(cliente);
                     } else if (opcaoVendedor == 2) {
-                        System.out.println("Adicionando óculos ao estoque...");1
+                        //System.out.println("Adicionando óculos ao estoque...");1
                         // Aqui você pode implementar a lógica para adicionar óculos ao estoque
                     } else if (opcaoVendedor == 3) {
                         System.out.println("Digite o valor da venda:");
