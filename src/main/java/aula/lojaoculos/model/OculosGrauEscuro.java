5978631cc49c61c42f78691257dc0acfc35f98db
AThees <alvaro@thees.com.br>
2023-12-11 19:39:55 -0300

    fixed constructor parameters and inheritance

--- src/main/java/aula/lojaoculos/model/OculosGrauEscuro.java
+++ src/main/java/aula/lojaoculos/model/OculosGrauEscuro.java
@@ -1,11 +1,11 @@
 package aula.lojaoculos.model;
 
-public class OculosGrauEscuro extends OculosEscuros{
+public class OculosGrauEscuro extends OculosGrau{
     private boolean polarizado;
     String corDaLente;
 
-    public OculosGrauEscuro(String modelo, String marca, double preco, boolean polarizado, String corDaLente) {
-        super(modelo, marca, preco, polarizado, corDaLente);
+    public OculosGrauEscuro(String modelo, String marca, double preco, double grauEsquerda, double grauDireita, String tipoLente, boolean polarizado, String corDaLente) {
+        super(modelo, marca, preco, grauEsquerda, grauDireita, tipoLente);
         this.polarizado = polarizado;
         this.corDaLente = corDaLente;
     }
