39d213e1d8e5254d481037f3bc28cd676e9e853b
AThees <alvaro@thees.com.br>
2023-12-11 19:38:32 -0300

    Changed attributes type from float to double

--- src/main/java/aula/lojaoculos/model/OculosGrau.java
+++ src/main/java/aula/lojaoculos/model/OculosGrau.java
@@ -1,11 +1,11 @@
 package aula.lojaoculos.model;
 
 public class OculosGrau extends Oculos {
-    private float grauEsquerda;
-    private float grauDireita;
+    private double grauEsquerda;
+    private double grauDireita;
     private String tipoLente;
 
-    public OculosGrau(String modelo, String marca, double preco, float grauEsquerda, float grauDireita, String tipoLente) {
+    public OculosGrau(String modelo, String marca, double preco, double grauEsquerda, double grauDireita, String tipoLente) {
         super(modelo, marca, preco);
         this.grauEsquerda = grauEsquerda;
         this.grauDireita = grauDireita;
