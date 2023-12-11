c8dbf58c03a64cae16ce48a6fffe9dd082b50b35
AThees <alvaro@thees.com.br>
2023-12-11 19:38:08 -0300

    Added "codigo" attribute

--- src/main/java/aula/lojaoculos/model/Oculos.java
+++ src/main/java/aula/lojaoculos/model/Oculos.java
@@ -4,6 +4,7 @@ public abstract class Oculos {
     protected String modelo;
     protected String marca;
     protected double preco;
+    protected int codigo;
 
     public Oculos(String modelo, String marca, double preco) {
         this.modelo = modelo;
