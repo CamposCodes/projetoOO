a4cac5273c7b8f57fda53151fcd6226f7db6e763
AThees <alvaro@thees.com.br>
2023-12-13 21:52:04 -0300

    update

--- src/main/java/aula/lojaoculos/controller/CadastraCliente.java
+++
@@ -1,9 +0,0 @@
-package aula.lojaoculos.controller;
-import aula.lojaoculos.view.ViewCadastraCliente;
-
-
-public class CadastraCliente {
-    public CadastraCliente(){
-        new ViewCadastraCliente();
-    }
-}
