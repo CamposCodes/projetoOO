0b309ec026f535828ddb826cfd40a0700896ec57
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2023-12-15 10:26:19 -0300

    Created classes for removal and editing for each object

---
+++ src/main/java/aula/lojaoculos/view/updates/ViewEditaremoveCliente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula.lojaoculos.view.updates;
+
+/**
+ *
+ * @author gcamp
+ */
+public class ViewEditaremoveCliente {
+    
+}
