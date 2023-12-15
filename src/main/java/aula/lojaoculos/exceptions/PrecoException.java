9ba5936403c061ef264d6e4ff881a5e3aad7518d
AThees <alvaro@thees.com.br>
2023-12-15 00:09:29 -0300

    Created Preco Exception

---
+++ src/main/java/aula/lojaoculos/exceptions/PrecoException.java
@@ -0,0 +1,10 @@
+package aula.lojaoculos.exceptions;
+
+public class PrecoException extends Exception{
+    public PrecoException() {
+    }
+
+    public PrecoException(String message) {
+        super(message);
+    }
+}
