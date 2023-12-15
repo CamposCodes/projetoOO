daced5519db4c9a64acae675ae2416200b91336c
AThees <alvaro@thees.com.br>
2023-12-15 00:13:41 -0300

    Changed "polarizado" attribute from boolean to String

--- src/main/java/aula/lojaoculos/model/OculosEscuros.java
+++ src/main/java/aula/lojaoculos/model/OculosEscuros.java
@@ -1,10 +1,10 @@
 package aula.lojaoculos.model;
 
 public class OculosEscuros extends Oculos{
-    private boolean polarizado;
+    private String polarizado;
     private String corDaLente;
 
-    public OculosEscuros(String modelo, String marca, double preco, boolean polarizado, String corDaLente) {
+    public OculosEscuros(String modelo, String marca, double preco, String polarizado, String corDaLente) {
         super(modelo, marca, preco);
         this.polarizado = polarizado;
         this.corDaLente = corDaLente;
--- src/main/java/aula/lojaoculos/model/OculosGrauEscuro.java
+++ src/main/java/aula/lojaoculos/model/OculosGrauEscuro.java
@@ -1,10 +1,10 @@
 package aula.lojaoculos.model;
 
 public class OculosGrauEscuro extends OculosGrau{
-    private boolean polarizado;
+    private String polarizado;
     String corDaLente;
 
-    public OculosGrauEscuro(String modelo, String marca, double preco, double grauEsquerda, double grauDireita, String tipoLente, boolean polarizado, String corDaLente) {
+    public OculosGrauEscuro(String modelo, String marca, double preco, double grauEsquerda, double grauDireita, String tipoLente, String polarizado, String corDaLente) {
         super(modelo, marca, preco, grauEsquerda, grauDireita, tipoLente);
         this.polarizado = polarizado;
         this.corDaLente = corDaLente;
