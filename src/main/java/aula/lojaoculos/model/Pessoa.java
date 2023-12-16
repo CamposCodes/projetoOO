4c76d1a6d50648e8c740a30cc3e0e3fa99e1054d
Álvaro Thees <alvaro@thees.com.br>
2023-12-16 10:03:31 -0300

    Added getters and setters to person

--- src/main/java/aula/lojaoculos/model/Pessoa.java
+++ src/main/java/aula/lojaoculos/model/Pessoa.java
@@ -5,6 +5,39 @@ import java.util.Date;
 public abstract class Pessoa {
     protected String nome;
     protected Date dtNasc;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDtNasc() {
+        return dtNasc;
+    }
+
+    public void setDtNasc(Date dtNasc) {
+        this.dtNasc = dtNasc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
     protected String email;
     protected String cpf;
     protected String telefone;
