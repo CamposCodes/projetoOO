e1ab26c90561f99b8e397b5becfd27983f7e8738
Álvaro Thees <alvaro@thees.com.br>
2023-12-16 12:49:16 -0300

    Created new valor minimo exception

---
+++ src/main/java/aula/lojaoculos/exceptions/ValorMinimoException.java
@@ -0,0 +1,10 @@
+package aula.lojaoculos.exceptions;
+
+public class ValorMinimoException extends Exception{
+    public ValorMinimoException() {
+    }
+
+    public ValorMinimoException(String message) {
+        super(message);
+    }
+}
